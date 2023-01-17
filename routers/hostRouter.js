0325432ca742aedf233018ddc810f1c5a569fe3a
Yovel Elad <yovel@gmail.com>
2023-01-17 21:19:15 +0200

    add middleware

--- routers/hostRouter.js
+++ routers/hostRouter.js
@@ -1,10 +1,10 @@
 const { Router } = require('express');
 const { hostController } = require('../controllers/hostController');
 const hostRouter = new Router();
-const auth= require("../middlewares/auth");
-module.exports = { hostRouter};
+const auth = require("../middlewares/auth");
+module.exports = { hostRouter };
 
-hostRouter.post('/' ,hostController.createHost);
-hostRouter.delete('/' ,hostController.deleteHost);
-hostRouter.get('/' ,hostController.getAllProblems);
+hostRouter.post('/', auth, hostController.createHost);
+hostRouter.delete('/', auth, hostController.deleteHost);
+hostRouter.get('/', auth, hostController.getAllProblems);
 
