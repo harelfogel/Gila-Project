3994869d0c333553580560517edac5fcf50f271a
Ofir <ofir.duchovne@gmail.com>
2023-01-24 22:48:32 +0200

    ofir: added close problem functionality with hostname validation

--- server.js
+++ server.js
@@ -3,12 +3,16 @@ require('dotenv').config();
 const { conversation } = require('@assistant/conversation')
 const port = 443;
 const bodyParser = require('body-parser')
+const axios = require("axios");
 
+const _ = require('lodash');
 const https = require('https');
 const fs = require('fs');
 const { hostController } = require("./controllers/hostController");
 const { groupController } = require("./controllers/groupsController");
-const { getGroupIdByName, getAuth, getHostIdByName } = require("./utils/utils");
+const { addStat, readStats } = require("./utils/stats");
+const { getAuth,isHostExists } = require("./utils/utils");
+const { concat } = require("lodash");
 const CERT_PATH = '/etc/letsencrypt/live/gila.shenkar.cloud/fullchain.pem';
 const KEY_PATH = '/etc/letsencrypt/live/gila.shenkar.cloud/privkey.pem';
 
@@ -21,8 +25,9 @@ const testAuth= '8f774ea17b24a26fb8c0295dbc20786a';
 
 const problemsTest=async ()=>{
   const test=await hostController.listAllProblems('10084');
-  console.log({test:test.message})
+  console.log(await hostController.closeProblems('')); 
 }
+
 
 problemsTest();
 
@@ -56,6 +61,53 @@ app.handle('listProblems', async conv => {
   const response = await hostController.listAllProblems({params: conv.session.params});
   console.log({message: response.message})
   conv.add(response.message);
+});
+
+app.handle('closeProblemStart', async conv => {
+  console.log('*********closeProblemStart******');
+  // const response = await hostController.listAllClosableProblems({params: conv.session.params});
+  conv.session.params.problems = ['CPU usage is too high on Zabbix server [trapper #1] on "Zabbix server"', 'CPU usage is too high on Zabbix server [trapper #2] on "Zabbix server"'];
+  conv.session.params.problemIds = ['10084', '10085'];
+  conv.session.params.hostId = '10084';
+  conv.session.params.currentProblem = 0;
+  conv.add('<speak>' + conv.session.params.problems[0] + '<break time="600ms"/> Do you want to close it?' + '</speak>');
+  // console.log({message: response.message})
+  // conv.add(response.message);
+});
+
+app.handle('closeProblem', async conv => {
+  console.log('*********closeProblem******');
+  // const response = await hostController.listAllProblems({params: conv.session.params});
+  if(conv.intent.name === "yes") {
+    // const response = await hostController.closeProblems({hostId: conv.session.params.hostId, problemId: conv.session.params.problemIds[conv.session.params.currentProblem]});
+    conv.session.params = {}
+    conv.scene.next.name = 'MainMenu';
+    conv.add('<speak>'+'problem closed!' + '<break time="600ms"/>' + '</speak>');
+  } else {
+    conv.session.params.currentProblem++;
+    if(conv.session.params.currentProblem < conv.session.params.problems.length){
+      conv.scene.next.name = 'closeProblemYesNo';
+      conv.add('<speak>' + conv.session.params.problems[conv.session.params.currentProblem] + '<break time="600ms"/> Do you want to close it?' + '</speak>');
+    }else{
+      conv.scene.next.name = 'MainMenu';
+      conv.add('<speak> There are no more problems <break time="600ms"/></speak>');
+    }
+  }
+});
+
+app.handle("closeProblemSlotValidation", async conv => {
+  console.log('*********closeProblemSlotValidation******');
+  const hostName = conv.scene.slots['host_name'].value;
+  const auth = await getAuth();
+  const hostExists = await isHostExists(auth, hostName);
+  console.log({hostName, hostExists});
+  // make sure the host exists
+  if(!hostExists){
+    console.log('host does not exist');
+    conv.scene.slots['host_name'].status = 'INVALID';
+    conv.add(`<speak>Host ${hostName} does not exist, <break time="600ms"/> please try again </speak>`);
+  }
+
 });
 
 app.handle('ackProblem', conv => {
@@ -92,11 +144,18 @@ app.handle('createUser', async conv => {
   conv.add(resText);
 });
 
+expressApp.get('/healthcheck', (req, res) => {
+  console.log('*********healthcheck******');
+  res.send('Hello World!');
+  addStat("test");
+})
 
+expressApp.get('/stats', (req, res) => {
+  console.log('*********stats******');
+  res.send(readStats());
+})
 
 expressApp.post('/fulfillment', app);
-
-
 https.createServer({
   key: fs.readFileSync(KEY_PATH),
   cert: fs.readFileSync(CERT_PATH)
